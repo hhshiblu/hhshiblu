c16f17bb03576dec06f246b9dfeb46e745fa056b
hhshiblu <hhshiblu5555@gmail.com>
2023-02-12 22:44:14 +0600

    hhshiblu

--- readme/data.js
+++ readme/data.js
@@ -1,33 +1,35 @@
 module.exports = {
   intro: {
-    mail: 'sayad.echo@gmail.com',
-    website: "I don't have enough time to create a portfolio",
+    mail: 'hhshiblu5555@gmail.com',
+    website: "https://shiblu.vercel.app/",  
   },
+
 
   stack: require('./partials/stack'),
 
+  
   overview: {
     profileViews:
-      'https://komarev.com/ghpvc/?username=NazmusSayad&style=flat-square',
+      'https://komarev.com/ghpvc/?username=hhshiblu&style=flat-square',
     streakStats:
-      'https://github-readme-streak-stats.herokuapp.com?user=NazmusSayad&theme=onedark&date_format=M%20j%5B%2C%20Y%5D&dates=737373&ring=DD8484&fire=E25822&stroke=00000000&currStreakNum=DD0D4F&currStreakLabel=A6A6A6&border=00000000&background=161B22',
+      'https://github-readme-streak-stats.herokuapp.com?user=hhshiblu&theme=onedark&date_format=M%20j%5B%2C%20Y%5D&dates=737373&ring=DD8484&fire=E25822&stroke=00000000&currStreakNum=DD0D4F&currStreakLabel=A6A6A6&border=00000000&background=161B22',
     overviewStats:
-      'https://github.com/NazmusSayad/Git-Stats/raw/master/generated_images/overviewDarkMode.svg',
+      'https://github.com/hhshiblu/Git-Stats/raw/master/generated_images/overviewDarkMode.svg',
     languageStats:
-      'https://github.com/NazmusSayad/Git-Stats/raw/master/generated_images/languagesDarkMode.svg',
+      'https://github.com/hhshiblu/Git-Stats/raw/master/generated_images/languagesDarkMode.svg',
   },
 
   projects: require('./partials/projects'),
 
   metrics: {
     githubMetrics:
-      'https://github.com/NazmusSayad/Git-Stats/raw/master/github-metrics.svg',
+      'https://github.com/hhshiblu/Git-Stats/raw/master/github-metrics.svg',
     activityGraph:
-      'https://github-readme-activity-graph.cyclic.app/graph?username=NazmusSayad&bg_color=161B22&color=9e9e9e&line=bababa&point=a76c6c&area=true&hide_border=true&hide_title=true',
+      'https://github-readme-activity-graph.cyclic.app/graph?username=hhshiblu&bg_color=161B22&color=9e9e9e&line=bababa&point=a76c6c&area=true&hide_border=true&hide_title=true',
   },
 
   footer: {
-    buyMeCoffee: 'https://www.buymeacoffee.com/NazmusSayad',
+    buyMeCoffee: 'https://www.buymeacoffee.com/hhshiblu',
     buyMeCoffeeIcon:
       'https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png',
     gridSnake:
